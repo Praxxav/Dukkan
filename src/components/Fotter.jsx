9149afb80ab5a5cab4ec3b4ecab53a86f5191885
pranav kulkarni <pranavkulkarni33917@gmail.com>
2024-06-26 17:09:45 +0530

    first commit

---
+++ src/components/Fotter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Fotter() {
+    return (
+        <div className='flex justify-center items-center flex-col text-black-300 mb-6 gap-2'>
+            <div>
+                ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://x.com/PranavKulk61053">Pranav kulkarni</a>
+            </div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/Praxxav/Dukkan-Tailwind-UI">Get the source code</a></div>
+        </div>
+    )
+}
+
+export default Fotter
