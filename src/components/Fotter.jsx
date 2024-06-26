3bf989715f08eec35d7fe8a077230644843be981
pranav kulkarni <pranavkulkarni33917@gmail.com>
2024-06-26 22:29:27 +0530

    first commit

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -6,7 +6,7 @@ function Fotter() {
             <div>
                 ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://x.com/PranavKulk61053">Pranav kulkarni</a>
             </div>
-            <div  className='text-black-300 underline' ><a href="https://github.com/Praxxav/Dukkan-Tailwind-UI">Get the source code</a></div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/Praxxav/Dukkan">Get the source code</a></div>
         </div>
     )
 }
